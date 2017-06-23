5b7886e9e983d12078bfb4075454ec9985847628
Krishna <krishnaqmar@gmail.com>
2017-06-23 14:16:41 +0530

    project 5

---
+++ solutions/nprime.py
@@ -0,0 +1,42 @@
+""" Find next prime number 
+"""
+
+def is_prime(n):
+
+	if n == 2:
+		return True
+
+	if not n & 1:
+		return False
+
+	for x in range(3, int(n**0.5), 2):
+		if n%x == 0:
+			return False
+
+	return True
+
+
+
+
+quit = False
+start = 2
+prev = []
+while not quit:
+
+	user_input = input('n for next prime, q for quit')
+	print('Got user input')
+	if user_input == 'q':
+		print('user pressed q')
+		quit = True
+		break
+
+	if user_input == 'n':
+		print('user pressed n')
+		for i in range(start, 1000):
+			if is_prime(i) and i not in prev:
+				print(i)
+				prev.append(i)
+				break
+
+
+
