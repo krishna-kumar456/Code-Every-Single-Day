c93d29ebe3587980dd53098efe2a0d2cf047c1ab
Krishna <krishnaqmar@gmail.com>
2017-08-13 06:54:02 +0530

    euler complete

--- solutions/eulerpath.py
+++ solutions/eulerpath.py
@@ -4,30 +4,48 @@
 import graphfromlinks as G
 
 
-def is_connected(g):
-	""" A method to check if the vertices
-		of the graph are connected.
-	"""
-	for k, v in g:
-		if len(v) > 0:
-			return True
-
-
-
-
-
-
 if __name__ == "__main__":
 
 	g = { 	  "a" : ["d"],
-          "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
-          "e" : ["c"],
-          "f" : []
+		      "b" : ["c"],
+		      "c" : ["b", "c", "d", "e"],
+		      "d" : ["a", "c"],
+		      "e" : ["c"],
+		      "f" : []
         }
 
 	graph = G.Graph(g)
+
+	vertex_graph = graph.get_degree()
+
+	count_of_even_vertices = 0
+	count_of_odd_vertices = 0
+
+	for k, v in vertex_graph.items():
+
+		if v % 2 == 0:
+			count_of_even_vertices += 1
+
+		if v % 2 == 1:
+			count_of_odd_vertices += 1
+
+
+	if count_of_even_vertices == len(vertex_graph.keys()):
+		print('Euler cycle and path found in Graph')
+
+	else:
+		print('No Euler cycle found in Graph')
+
+	if count_of_odd_vertices == 2 and count_of_even_vertices == len(vertex_graph.keys()) - 2:
+		print('Euler path found in Graph')
+	else:
+		print('No Euler path found in Graph')
+
+
+
+
+
+
 
 
 
