10f2dea9733a52164e19dbbae9f48d16e14f9e03
Krishna <krishnaqmar@gmail.com>
2017-08-19 06:56:10 +0530

    inverted index complete

--- solutions/invertedindex.py
+++ solutions/invertedindex.py
@@ -2,52 +2,61 @@
 """
 from collections import defaultdict
 
-def create_index (data):
+
+
+def create_index(data):
 	""" Creates the Invertex index using the
 		default dictionary data structure
 		within the python collections.
-	"""
-
-	index = defaultdict(list)
+	"""	
+	sIndex = defaultdict(list)
 	for i, tokens in enumerate(data):
-		print('i', i, 'tokens', tokens)
-		index[tokens].append(i)
-	return index
+		for token in tokens:
+			for t in token:
+				sIndex[t].append(i)
+	return sIndex		
 
 
 
-def read_file(file):
+def read_file(files):
 	""" Helper function to read text from a
 		file and then use the contents of 
 		the file to create an inverted index.
 	"""
 	try:
-		text_file = open(file, 'r')
+		content = []
+		for file in files:
+			print(file)
+			with open(file) as f:
+				content.append(f.readlines())
+
+		result = []
+		for c in content:
+			result.append([x.split() for x in c])
 	
 	except e:
 		print('Something went wrong with opening the file', e)
 
-	return text_file.split()
+	return result
 
 
-def search_text(file, query):
+def search_text(files, query):
 	""" Searches the user inputed text 
 		within the Invertex Index.
 	"""
-
-	index = create_index(read_file(file))   ##Modifiy this to accept more than 1 file.
-
+	index = create_index(read_file(files))
 	return index[query]
 
 
 
 if __name__ == '__main__':
 
-	number_of_files = int('Please enter the number of files: ')
+	number_of_files = input('Please enter the number of files: ')
+	
 	files = []
 
-	for number in range(number_of_files):
-		file_name = input('Please enter the file name for file ' +number+ ': ')
+	for number in range(int(number_of_files)):
+		file_name = input('Please enter the file name for file: ')
 		files.append(file_name)
 
 
