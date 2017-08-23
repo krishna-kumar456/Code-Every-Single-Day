ab807e70a4ebf4d0e9abd05448d9eb21f4554a68
Krishna <krishnaqmar@gmail.com>
2017-08-23 08:07:20 +0530

    pig latin complete

--- solutions/piglatin.py
+++ solutions/piglatin.py
@@ -17,6 +17,25 @@ def convert_to_pig_latin(word):
 	if char_list[0] in vowels:
 		resultant_word = word + 'way'
 
+	""" Consonant Rule.
+	"""
+
+	if char_list[0] not in vowels:
+		resultant_word = ''.join(char_list[1:]) + str(char_list[0]) + 'a'
+
+	""" Consonant Cluster Rule.
+	"""
+
+	if char_list[0] == 'c' and char_list[1] == 'h':
+		resultant_word = ''.join(char_list[2:]) + char_list[0] + char_list[1] + 'ay'
+
+	elif char_list[0] == 's' and char_list[1] == 'h' or char_list[1] =='t' or char_list[1] == 'm':
+		resultant_word = ''.join(char_list[2:]) + char_list[0] + char_list[1] + 'ay'
+
+	elif char_list[0] == 't' and char_list[1] == 'h':
+		resultant_word = ''.join(char_list[2:]) + char_list[0] + char_list[1] + 'ay'
+
+
 
 
 
