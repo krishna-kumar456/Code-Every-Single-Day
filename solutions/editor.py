d5c68d4e0ba4dd064d4c8f032a673190b0c6546c
Krishna <krishnaqmar@gmail.com>
2017-08-30 08:19:10 +0530

    gui notepad

--- solutions/editor.py
+++ solutions/editor.py
@@ -3,15 +3,26 @@
 
 from appJar import gui
 
-def buttonClick(btnName):
+def openButtonClick(btnName):
 	""" Invoker function that is called whenever a button is clicked.
 	"""
-	print(btnName + "was clicked!")
+	print(btnName)
+	app.openBox(title=None, dirName=None, fileTypes=None, asFile=False)
+
+def saveButtonClick(btnName):
+	""" Invoker function that is called whenever a button is clicked.
+	"""
+
+	print(btnName)
+
 
 
 if __name__ == '__main__':
 
+	menu = ['Open', 'Save']
+
+	menuClickFunctions = [openButtonClick, saveButtonClick]
 	app = gui()
-	app.addButton("File", buttonClick)
+	app.addToolbar(menu, menuClickFunctions)
 	app.addScrolledTextArea('text')
 	app.go()
