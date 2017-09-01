c1d8bfade2483af05a3bf995307cd0b9db1e1ba5
Krishna <krishnaqmar@gmail.com>
2017-09-01 08:41:29 +0530

    Rssreader intro

---
+++ solutions/rssfeed.py
@@ -0,0 +1,12 @@
+import feedparser
+
+rss_feed_links = ['https://www.reddit.com/.rss']
+
+for feed_link in rss_feed_links:
+    d = feedparser.parse(feed_link)
+    print("Total entries: ", len(d.entries))
+    print("--------------------------------")
+    for entry in d.entries:
+        print("--------------------------------")
+        print("Title :", entry.title)
+        print("Link :", entry.link)
